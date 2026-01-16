680f9bc756d9d082720522c882ce72e6ccbcf1b2
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-01-16 20:21:58 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.cpp
@@ -0,0 +1,21 @@
+// Last updated: 1/16/2026, 8:21:56 PM
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        long long low = 1, high = x, ans = 0;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;      // possible answer
+                low = mid + 1;  // try bigger
+            } else {
+                high = mid - 1; // try smaller
+            }
+        }
+        return ans;
+    }
+};

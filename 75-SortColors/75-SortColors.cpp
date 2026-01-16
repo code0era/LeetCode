67a37fafe89f78afa49332f5ad86dbe35aceb994
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-01-16 20:21:55 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,23 @@
+// Last updated: 1/16/2026, 8:21:53 PM
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int  l = 0 ;
+        int  m = 0 ;
+        int  h = nums.size()-1 ;
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums[l] , nums[m]);
+                l++;
+                m++;
+                }
+            else if(nums[m] ==1){
+                m++;
+            }
+            else if(nums[m]==2){
+                swap(nums[m], nums[h]);
+                h--;
+            }
+        }
+    }
+};

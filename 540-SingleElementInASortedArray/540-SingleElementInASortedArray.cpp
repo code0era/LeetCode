1893e0dd614fabe6064028292dc20b2acc8b5486
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-01-16 20:21:04 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.cpp
@@ -0,0 +1,28 @@
+// Last updated: 1/16/2026, 8:21:02 PM
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0 ;
+        int h = n-1 ;
+        // specific cases
+        if(n==1){return nums[0];}
+        if(nums[0]!= nums[1]){return nums[0];}
+        if(nums[n-1]!= nums[n-2]){return nums[n-1];}
+
+        // bs
+        while(l<=h){
+            int  m = (l+h)/2;
+            if(nums[m] != nums[m-1] && nums[m]!= nums[m+1]){
+                return nums[m];
+            }
+            if((m%2 ==1 && nums[m] ==nums[m-1] )|| (m%2 ==0 && nums[m] ==nums[m+1] ) ){
+                l = m+1;
+            }
+            else{
+                h = m-1;
+            }
+        }
+        return -1;
+    }
+};

f8da4e33198c22d5314de1da366e0775060e5642
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-05-05 08:43:41 -0700

    Create 1984-MaximumDistanceBetweenAPairOfValues

---
+++ 1984-MaximumDistanceBetweenAPairOfValues/1984-MaximumDistanceBetweenAPairOfValues.cpp
@@ -0,0 +1,26 @@
+// Last updated: 5/5/2026, 9:13:39 PM
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0;
+        int maxDist = 0;
+
+        while(i < n && j < m) {
+            if(nums1[i] <= nums2[j]) {
+                maxDist = max(maxDist, j - i);
+                j++;
+            }else {
+                i++;
+            }
+        }
+
+        return maxDist;
+    }
+};

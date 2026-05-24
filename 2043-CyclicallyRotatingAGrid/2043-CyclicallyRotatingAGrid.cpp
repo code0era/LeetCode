60ebad105e77badca7576928fc6d7e299a2e349b
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-05-24 09:59:25 +0530

    Create 2043-CyclicallyRotatingAGrid

---
+++ 2043-CyclicallyRotatingAGrid/2043-CyclicallyRotatingAGrid.cpp
@@ -0,0 +1,67 @@
+// Last updated: 5/24/2026, 9:59:21 AM
+class Solution {
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int layers = min(m, n) / 2;
+
+        for (int layer = 0; layer < layers; layer++) {
+
+            vector<int> elems;
+
+            int top = layer;
+            int left = layer;
+            int bottom = m - layer - 1;
+            int right = n - layer - 1;
+
+            // top row
+            for (int j = left; j <= right; j++)
+                elems.push_back(grid[top][j]);
+
+            // right col
+            for (int i = top + 1; i <= bottom - 1; i++)
+                elems.push_back(grid[i][right]);
+
+            // bottom row
+            for (int j = right; j >= left; j--)
+                elems.push_back(grid[bottom][j]);
+
+            // left col
+            for (int i = bottom - 1; i >= top + 1; i--)
+                elems.push_back(grid[i][left]);
+
+            int len = elems.size();
+
+            int rot = k % len;
+
+            vector<int> rotated(len);
+
+            for (int i = 0; i < len; i++) {
+                rotated[i] = elems[(i + rot) % len];
+            }
+
+            int idx = 0;
+
+            // top row
+            for (int j = left; j <= right; j++)
+                grid[top][j] = rotated[idx++];
+
+            // right col
+            for (int i = top + 1; i <= bottom - 1; i++)
+                grid[i][right] = rotated[idx++];
+
+            // bottom row
+            for (int j = right; j >= left; j--)
+                grid[bottom][j] = rotated[idx++];
+
+            // left col
+            for (int i = bottom - 1; i >= top + 1; i--)
+                grid[i][left] = rotated[idx++];
+        }
+
+        return grid;
+    }
+};

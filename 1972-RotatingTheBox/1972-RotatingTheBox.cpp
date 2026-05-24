4fad1aa98633bfe3c97777239a41828d64f948e3
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-05-24 09:59:31 +0530

    Create 1972-RotatingTheBox

---
+++ 1972-RotatingTheBox/1972-RotatingTheBox.cpp
@@ -0,0 +1,35 @@
+// Last updated: 5/24/2026, 9:59:28 AM
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size(), n = box[0].size();
+
+        // apply gravity
+        for (int i = 0; i < m; i++) {
+            int empty = n-1;
+
+            for(int j = n - 1; j >= 0; j--) {
+
+                if(box[i][j] == '*') {
+                    empty = j - 1;
+                }
+
+                else if(box[i][j] == '#') {
+                    swap(box[i][j], box[i][empty]);
+                    empty--;
+                }
+            }
+        }
+
+        // rotate
+        vector<vector<char>> ans(n, vector<char>(m));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                ans[j][m - 1 - i] = box[i][j];
+            }
+        }
+
+        return ans;
+    }
+};

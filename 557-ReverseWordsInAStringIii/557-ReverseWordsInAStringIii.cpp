48963e1c4c773d711073df31fefddd146e9fffed
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-06-14 09:38:15 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/14/2026, 9:38:13 AM
+class Solution {
+public:
+
+    string reverseWords(string s) {
+
+        int n = s.size();
+
+        int start =0 ;
+
+        for(int i = 0; i <= n; i++) {
+
+          
+            if(i == n || s[i] == ' ') {
+                reverse(s.begin() + start, s.begin() + i);
+
+      
+                start = i + 1;
+            }
+        }
+
+        return s;
+    }
+};

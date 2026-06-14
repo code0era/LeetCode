4d88e16a6840a996bf525981b89a4196cd4dd022
Shubham Yadav <140435929+code0era@users.noreply.github.com>
2026-06-14 09:37:58 +0530

    Create 816-DesignHashset

---
+++ 816-DesignHashset/816-DesignHashset.cpp
@@ -0,0 +1,22 @@
+// Last updated: 6/14/2026, 9:37:56 AM
+class MyHashSet {
+ public:
+  /** Initialize your data structure here. */
+  MyHashSet() : set(1000001) {}
+
+  void add(int key) {
+    set[key] = true;
+  }
+
+  void remove(int key) {
+    set[key] = false;
+  }
+
+  /** Returns true if this set contains the specified element */
+  bool contains(int key) {
+    return set[key];
+  }
+
+ private:
+  vector<bool> set;
+};
